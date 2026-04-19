94c7253d18965399c5b48cf6d752df34ff9f26a4
sy-00 <sylwia3p@wp.pl>
2026-04-19 12:36:44 +0200

    Update main2.c

--- main2.c
+++ main2.c
@@ -6,7 +6,7 @@ int main() {
     InitTimer0Match0(1000000); // budzik na 1mln mikrosekund - 1 sekundkę
     while (1) {
         WaitOnTimer0Match0(); // procesor w kółko sprawdza rejestr T0IR (flagę)
-        LedStepRight(); // co sekundkę lecimy ledem w prawo
+        LedStepLeft(); // co sekundkę lecimy ledem w lewo
     }
 }
-// czyli zobaczymy w nieskończoność zapalające się co sekundę ledy idące w prawo!!!!!!
+// czyli zobaczymy w nieskończoność zapalające się co sekundę ledy idące w lewo!!!!!!
