4e3a9f980cfb992d15e649562fc3b03e953e4be9
sy-00 <sylwia3p@wp.pl>
2026-04-19 12:26:24 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 
 int main() {
     LedInit();
-    InitTimer0();
+    InitTimer0(); //włączamy tylko timer, nie waidomo na ile (tak jak w punkcie 2 gdzie ustawiamy sobie InitTimer0Match0(1000000))
     while (1) 
     {
-        WaitOnTimer0(1000000);
+        WaitOnTimer0(1000000); // tutaj ustawiamy czas (w 2 ta funkcja nic nie przyjmuje) - tutaj ręcznie mówimy licznikowi żeby się zerował i stał aż nie naliczy 1 sekundki
         LedStepRight();
     }
 }
